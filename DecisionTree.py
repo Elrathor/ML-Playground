4b1eadbbffc5f1d7d89db9abf111573457073ae0
Tobias Boyer <tobias.boyer@emp.de>
2023-02-23 22:29:40 +0100

    Add DecisionTree Example

---
+++ DecisionTree.py
@@ -0,0 +1,17 @@
+from sklearn import tree
+
+if __name__ == '__main__':
+    inputValues = [
+        [0, 0],  # => 0
+        [1, 1],  # => 1
+        [2, 2],  # => 2
+        [3, 3]   # => 3
+    ]
+    targetValues = [0, 1, 2, 3]
+    treeClassifier = tree.DecisionTreeClassifier()
+    treeClassifier = treeClassifier.fit(inputValues, targetValues)
+
+    result = treeClassifier.predict([[2., 2.]])
+
+    if result == [2]:
+        print("Hello World 😎")
